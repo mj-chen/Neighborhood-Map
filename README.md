bdc15e11293afd6f599dcd347def3ac295b3bf17
Meijuan <mjchen8873@gmail.com>
2017-11-03 13:43:05 +0000

    add Readme and update js

---
+++ README.md
@@ -0,0 +1,18 @@
+App overview - what is it? 
+	Neighborhood Map APP is a single page application featuring a map of Greenwich in London and listing some interesting places to visit in this neighborhood such as restaurant, gym, attraction and leisure park. It allows you to easily and quickly know Greenwich neighborhood and obtain detailed information about various places you are interested in. You save time by not needing to search information from many different sites. It can be used in both your PC and smartphone. 
+
+APP installation - how to get it?
+	. Either download or clone a zip of this repository at https://github.com/mj-chen/Neighborhood-Map.
+	. Navigate to the cloned/downloaded directory.
+	. Simply open index.html with your favorite browser and it will be ready to use.
+
+APP functions - how to use it?
+	. The application will load by default a Google map with a bunch of markers indicating all the interesting places in Greewich. 
+	. If you use the application in your PC, a list featuring the name of all the marked places will be loaded instantly. If you are on your smart phone with small screen, the list will be hide by defaut. Click the hamburger menu icon to hide or show the list. 
+	. Type the name of the place you want to search in the text input area. The first letters in each word should be uppercase. The application will filter and update in real time both the list and the markers according to your input, displaying only the search results. 
+	. By clicking either the place name in the list view or the corresponding marker on the map, an information window will be opened just above the marker. We utilise here Foursquare API to retrieve detailed information about this place.
+	. The zoom button will bring you closer to a selected location. Click the place name in the list view and then click the zoom button, the map will be zoomed into the place. If you click directly the zoom button without clicking a name place first, it's the first place in the list view that will be selected automatically. 
+
+
+
+
--- js/app.js
+++ js/app.js
@@ -1,44 +1,167 @@
 const Model ={
 	places:[
-			{place:'Royal Observatory Greenwich', coor:{lat:51.477191, lng:-0.000662}},
-			{place:"Bill'\s Greenwich Restaurant", coor:{lat:51.480758, lng:-0.009123}},
-			{place:"Greenwich Market", coor:{lat:51.481678, lng:-0.009076}},
-			{place:"The Coriander Indian Restaurant",coor:{lat:51.484716,lng:0.016883}},
-			{place:"Better Gym", coor:{lat:51.485565, lng:0.008553}},
-			{place:"Nova Medi Spa", coor:{lat:51.482191,lng:-0.009745}}
+			{place:"Greenwich Pier", coor:{lat:51.483653,lng:-0.009482}},
+			{place:"Nauticalia Greenwich", coor:{lat:51.481008, lng:-0.008228}},
+			{place:"Greenwich Market", coor:{lat:51.481557, lng:-0.009097}},
+			{place:"Better Gym Greenwich Center ", coor:{lat:51.485568, lng:0.008575}},
+			{place:"The Gipsy Moth Restaurant&Bar", coor:{lat:51.482191, lng:-0.009745}},
+			{place:"O2", coor:{lat:51.502118,lng:0.004838}},
+			{place:"Cutty Sark", coor:{lat:51.482847,lng:-0.009589}},
+			{place:"Royal Observatory Greenwich", coor:{lat:51.477819,lng:-0.001520}},
+			{place:"Paul Rhodes Bakery", coor:{lat:51.482037, lng:-0.008915}},
+			{place:"Greenwich Park", coor:{lat:51.476916,lng:0.001443}},
+			{place:"Island Gardens Park",coor:{lat:51.486911,lng:-0.008402}},
+			{place:"Pure Gym", coor:{lat:51.476164,lng:-0.018393}}
 		],
-	markers:[]
-};
+	markers:[],
+	getInfo: function(position,win,mar){
+		win.setMap(null);
+		Model.content = "";
+		//win.setContent("");
+		$.ajax({
+			method:"GET",
+			url:"https://api.foursquare.com/v2/venues/search",
+			data:{
+				client_id: 'FMFZ44MJ13REDKQWZ50WY11KGIFHNU50DD1JZQCOLYRLY2BZ',
+    			client_secret: '4ESYZKTK124LY5ATDXALNVB024SQILBMELDOQUMXL02ERJW4',
+    			ll:`${position.lat()},${position.lng()}`,
+    			v:'20170801',
+    			limit:1
+			},
+			success: function(response,status) {
+				console.log(response);
+				if (response.response.venues[0].id){           
+					$.ajax({
+						method:'GET',
+						url:'https://api.foursquare.com/v2/venues/'+ response.response.venues[0].id,
+						data:{
+							client_id: 'FMFZ44MJ13REDKQWZ50WY11KGIFHNU50DD1JZQCOLYRLY2BZ',
+	    					client_secret: '4ESYZKTK124LY5ATDXALNVB024SQILBMELDOQUMXL02ERJW4',
+	    					v:'20170801',
+	    					limit:1
+						},
+						success: function(response,status){
+							let venue = response.response.venue;
+							if (venue.name) {
+								if (venue.url) {
+									Model.content += `<div><a href="${venue.url}"><h4>${venue.name}<h4></a>`;
+								}else{
+									Model.content += `<div><h4>${venue.name}</h4>`;
+								}		
+							}
+							if (venue.contact.formattedPhone) {
+								Model.content += `<p>Contact Num: ${venue.contact.formattedPhone}</p>`;
+							}
+							if (venue.contact.twitter) {
+								Model.content +=`<p>Twitter: ${venue.contact.twitter}</p>`;
+							}
+							if (venue.contact.facebookName) {
+								Model.content += `<p>Facebook: ${venue.contact.facebookName}</p>`;
+							}
 
+							if (venue.hours) {
+								let openhours = `Openhours:<ul>`;
+								for(i=0; i<venue.hours.timeframes.length; i++){
+									openhours += `<li>${venue.hours.timeframes[i].days} <span>  <span> ${venue.hours.timeframes[i].open[0].renderedTime}</li>`;
+								}
+								openhours +=`</ul>`;
+								Model.content += openhours;
+							}
+							if (venue.rating) {
+								Model.content += `<p>Rating: ${venue.rating}</p>`;
+							}
+							if (venue.bestPhoto) {
+								let url = venue.bestPhoto.prefix + '300x300' + venue.bestPhoto.suffix;
+								Model.content += `<img src="${url}">`
+							}
+							Model.content += `<p><img src="img/foursquare.png"> <span style="color:blue;font-weight:900;font-size:1.2em;">Foursquare<span> <p></div>`
+							win.setContent(Model.content);
+							win.open(map,mar);
+						},
+						error:function(jqXHR){
+							let msg = 'The information about this place can\'t be loaded!\n\n';
+							msg += jqXHR.responseJSON.meta.errorDetail;
+							alert(msg);
+						}
+					});
+				}
+			},
+			error:function(jqXHR,textStatus){
+				 let msg = 'The information about this place can\'t be loaded!\n\n';
+		        if (jqXHR.status === 0) {
+		            msg += 'Not connect. Verify Network.';
+		        } else if (jqXHR.status == 404) {
+		            msg += 'Requested page not found. [404]';
+		        } else if (jqXHR.status == 500) {
+		            msg += 'Internal Server Error [500].';
+		        } else if (textStatus === 'parsererror') {
+		            msg += 'Requested JSON parse failed.';
+		        } else if (textStatus === 'timeout') {
+		            msg += 'Time out error.';
+		        } else if (textStatus === 'abort') {
+		            msg += 'Ajax request aborted.';
+		        } else {
+		            msg += 'Uncaught Error.\n' + jqXHR.responseJSON.meta.errorDetail + textStatus;
+		        }
+		        alert(msg);
+		        console.log(textStatus);
+			}
+		});
+	},
+	content:"",
+	window:[]
+};
 
 function appViewModel(){
 	let self = this;
 	self.toggleMenu = function(){
 		$('body').toggleClass('menu-hidden');
 	};
-	self.inputPlace = ko.observable("");
+	self.inputPlace = ko.observable();
 	self.selectedPlaces = ko.observableArray(Model.places);
-	self.filter = function(){
-		let searchedplace = self.inputPlace();
-		let upperCasePlaceArray = self.standardString(searchedplace);
-		console.log(upperCasePlaceArray);
-		let tempArray = [];
-		Model.places.forEach((ele)=>{
-			let include = false;
-			for (let i=0; i<upperCasePlaceArray.length; i++){
-				if (ele.place.includes(upperCasePlaceArray[i])){
-					include = true;
-					break;
-				}	
+	self.currentmarker;
+	self.search = function(data,event){
+		
+		if(event.key === 'Backspace'){
+			self.reset();
+		}else {
+	     	if(self.inputPlace()){
+				self.filter();
+				self.animateMarkers(Model.markers);
 			}
-			if(include){
-				tempArray.push(ele);
-			}	
-		});
-		self.selectedPlaces(tempArray);
-		self.updateMarkers(Model.markers, self.selectedPlaces());
-		self.animateMarkers(Model.markers);
+		}
+	   	return true;
 	};
+
+	self.filter = function(){
+		let temp = [];
+		let searchedplace = self.inputPlace();
+			for (let i=0; i<self.selectedPlaces().length; i++){
+				let place = self.selectedPlaces()[i];
+				let str = place.place;
+				let n = str.search(searchedplace);
+				if(n !== -1){
+					temp.push(place);
+				}
+			}
+			self.selectedPlaces(temp);
+			self.updateMarkers(Model.markers, self.selectedPlaces());
+	};
+
+	self.reset = function(){
+		console.log('back')
+		self.selectedPlaces(Model.places);
+		self.filter();
+		map.setCenter({lat:51.48, lng:-0.00});
+		map.setZoom(15);
+	}
+
+	self.zoom = function(){
+		let latlng = self.selectedPlaces()[0].coor;
+		console.log(latlng)
+		map.setCenter(latlng);
+		map.setZoom(18);
+	}
 
 	self.standardString = function(string){
 		let stringArray = string.toLowerCase().split(" ");
@@ -49,9 +172,14 @@ function appViewModel(){
 		})
 		return stringArray;
 	};
-	self.reset = function(){
-		self.selectedPlaces(Model.places);
-		self.updateMarkers(Model.markers, Model.places);
+
+	self.defineSelectedPlace = function(place){
+		self.selectedPlaces([place]);
+		self.inputPlace(place.place);
+		self.updateMarkers(Model.markers, self.selectedPlaces());
+		self.animateMarkers(Model.markers);
+		console.log(self.currentmarker);
+		Model.getInfo(self.currentmarker.getPosition(),Model.window[0],self.currentmarker);
 	};
 
 	self.makeMarkers = function(markerArray,placeArray){
@@ -73,6 +201,7 @@ function appViewModel(){
 				let marker = markerArray[j];
 				if(marker.title === selectedPlace.place){
 					marker.setMap(map);
+					self.currentmarker=marker;
 					break;
 				}
 			}
@@ -111,17 +240,18 @@ function initMap(){
 		fullscreenControl:true
 	};
 	map = new google.maps.Map(document.querySelector('#map'), mapOptions);
+	let bounds = new google.maps.LatLngBounds();
 	new appViewModel().makeMarkers(Model.markers, Model.places);
-
+	Model.window.push(new google.maps.InfoWindow());
 	Model.markers.forEach(marker =>{
+		bounds.extend(marker.position);
 		marker.addListener('click', function(){
-			this.setAnimation(google.maps.Animation.BOUNCE);
+			marker.setAnimation(google.maps.Animation.BOUNCE);
+			Model.getInfo(marker.getPosition(),Model.window[0],marker);
 		});
-
 		marker.addListener('mouseout', function(){
 			this.setAnimation(null);
 		})
+		map.fitBounds(bounds);
 	})
-
-	
 };
